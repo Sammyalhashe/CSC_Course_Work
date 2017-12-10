30d6c9d0603abc65870b91d4ca28205c20a1e7a9
Sammyalhashe <sammy.alhashemi@mail.utoronto.ca>
2017-12-09 16:08:27 -0800

    Hourglass question

---
+++ findMaxHourglass.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static int maxHourglass(int[][] arr, int n, int m) {
+        int maxhour = -100;
+        int sum;
+        for (int i = 0; i <= n / 2; i++) {
+            for (int j = 0; j <= m / 2; j++) {
+                sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
+                if (sum > maxhour) {maxhour = sum;}
+            }
+        }
+        return maxhour;
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int arr[][] = new int[6][6];
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+        int res = maxHourglass(arr, 6, 6);
+        System.out.println(res);
+    }
+}
+
