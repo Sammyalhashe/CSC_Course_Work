5a844f9dfaef07577c1e29616a412701cbda6deb
Sammyalhashe <sammy.alhashemi@mail.utoronto.ca>
2017-12-11 22:42:19 -0800

    balancedBrackets.java added

--- findMaxHourglass.java
+++ findMaxHourglass.java
@@ -9,8 +9,8 @@ public class Solution {
     public static int maxHourglass(int[][] arr, int n, int m) {
         int maxhour = -100;
         int sum;
-        for (int i = 0; i <= n / 2; i++) {
-            for (int j = 0; j <= m / 2; j++) {
+        for (int i = 0; i <= n - 3; i++) {
+            for (int j = 0; j <= m - 3; j++) {
                 sum = arr[i][j] + arr[i][j + 1] + arr[i][j + 2] + arr[i + 1][j + 1] + arr[i + 2][j] + arr[i + 2][j + 1] + arr[i + 2][j + 2];
                 if (sum > maxhour) {maxhour = sum;}
             }
